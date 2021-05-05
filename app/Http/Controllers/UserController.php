69a07375050939ec1be27e67b842f2129391b643
Matheus de Araujo <matheus.encode@gmail.com>
2021-05-04 22:41:22 -0300

    Inicio do Projeto

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        User::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Http\Controllers\UserController;
+
+Route::apiResource('user', UserController::class);
